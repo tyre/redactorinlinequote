409a4ed6c83cb44a161445271d64b9c468dc9abf
Chris Maddox <tyre77@gmail.com>
2017-05-17 16:06:59 -0700

    move to dropdown, add remove

--- redactorinlinequote/resources/js/redactor-inline-quote.js
+++ redactorinlinequote/resources/js/redactor-inline-quote.js
@@ -3,20 +3,33 @@ if (!RedactorPlugins) var RedactorPlugins = {};
 RedactorPlugins.inlineQuote = function() {
   return {
     init: function() {
-      var button = this.button.add('my-button', 'Inline Quote');
-      this.button.addCallback(button, this.inlineQuote.show);
+      var button = this.button.add('inline-quote', '“‘’”');
+      this.button.addDropdown(button, {
+        point1: { title: "“Double quote”", func: this.inlineQuote.formatDouble },
+        point2: { title: "‘Single quote’", func: this.inlineQuote.formatSingle },
+        point3: { title: "<em>remove</em>", func: this.inlineQuote.removeFormatting }
+      });
     },
-    format: function() {
-      if (this.selection.is()) {
-        var text = this.selection.text();
-        this.insert.set("<q>“" + text + "”</q>");
+    removeFormatting: function() {
+      var parent = this.selection.parent();
+      if (parent && parent.tagName == "Q") {
+        var quoteTag = $(parent).eq(0);
+        var quoteText = quoteTag.html();
+        var newText = quoteText.substr(1, quoteText.length - 2);
+        var newParentHtml = quoteTag.parent().html().replace(quoteTag[0].outerHTML, newText);
+        quoteTag.parent().html(newParentHtml);
       }
     },
-    removeFormat: function() {
-      var selectedElement = this.selection.get();
-      if (selectedElement.prop("tagName") === "Q") {
-        var text = this.selectedElement.innerHtml();
-        this.selection.set(text.substr(1, text.length - 1));
+    formatDouble: function() {
+      if (this.selection.is()) {
+        var html = this.selection.html();
+        this.selection.replace("<q>“" + html + "”</q>");
+      }
+    },
+    formatSingle:function(formatFunc) {
+      if (this.selection.is()) {
+        var html = this.selection.html();
+        this.selection.replace("<q>‘" + html + "’</q>");
       }
     }
   };
